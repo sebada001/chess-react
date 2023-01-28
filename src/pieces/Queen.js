b7a949d9ad1b9e148beabaf99fe2565d82e80970
Sebastian Schumacher <sebastianschumacher10@gmail.com>
2023-01-28 13:58:22 -0400

    queen moves

--- src/pieces/Queen.js
+++ src/pieces/Queen.js
@@ -1,20 +1,35 @@
 import queen_dark from "../images/Chess_qdt45.svg";
 import queen_light from "../images/Chess_qlt45.svg";
+import { diagonalOpponents, diagonalMoves } from "../moves/diagonal_moves";
+import { straightMoves, straightOpponents } from "../moves/straight_moves";
 import { nanoid } from "nanoid";
 
-// const Queen = (color, coord) => {
-//   let img = "";
-//   img = color === "black" ? queen_dark : queen_light;
-//   const display = () => img;
-//   let coordinate = coord;
-//   const position = (pos) => (coordinate = pos);
-//   return { display, position };
-// };
 function Queen(color, coord) {
   this.color = color;
+  this.type = "queen";
   this.coord = coord;
   this.image = color === "black" ? queen_dark : queen_light;
   this.id = nanoid();
 }
 
+function calculateQueenMoves(piece, board) {
+  let allMoves = [];
+  let movements = [];
+  let attacks = [];
+
+  movements = [
+    ...straightMoves(piece.color, piece.type, piece.coord, board),
+    ...diagonalMoves(piece.color, piece.type, piece.coord, board),
+  ];
+  attacks = [
+    ...diagonalOpponents(piece.color, piece.type, piece.coord, board),
+    ...straightOpponents(piece.color, piece.type, piece.coord, board),
+  ];
+  allMoves = [...movements, ...attacks];
+
+  return allMoves;
+}
+
 export default Queen;
+
+export { calculateQueenMoves };
--- src/utility/moves_dict.js
+++ src/utility/moves_dict.js
@@ -2,11 +2,13 @@ import { calculatePawnMoves } from "../pieces/Pawn";
 import { calculateBishopMoves } from "../pieces/Bishop";
 import { calculateKingMoves } from "../pieces/King";
 import { calculateRookMoves } from "../pieces/Rook";
+import { calculateQueenMoves } from "../pieces/Queen";
 
 const movesDict = {
   pawn: calculatePawnMoves,
   bishop: calculateBishopMoves,
   king: calculateKingMoves,
   rook: calculateRookMoves,
+  queen: calculateQueenMoves,
 };
 export { movesDict };
